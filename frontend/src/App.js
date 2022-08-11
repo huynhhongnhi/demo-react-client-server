ce960d9813684c9fb5d2e59b5605695747eb9952
Huynh Hong Nhi <>
2022-08-11 17:27:04 +0700

    build structure basic

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Layout from "./components/Layout/Layout";
+
+import "./assets/vendor/aos/aos.css";
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
+import "./assets/vendor/glightbox/css/glightbox.min.css";
+import "./assets/vendor/swiper/swiper-bundle.min.css";
+
+import "./assets/css/style.css";
+
+function App() {
+  return (
+    <Layout></Layout>
+  );
+}
+
+export default App;
